c499d3e97811a4b98c63b74aed9ee145eaabfec8
Ajay <ajaylearner01@gmail.com>
2025-08-11 07:45:22 +0530

    k8

---
+++ 80-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07396722PJPPRJILIZMP"
+}
+
+variable "zone_name" {
+    default = "ajay6.space"
+}
